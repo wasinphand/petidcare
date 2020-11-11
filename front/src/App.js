53f06e9158922b636d46a2d68468fb7b65359f77
SornrasakC <43643389+SornrasakC@users.noreply.github.com>
2020-11-11 20:08:35 +0700

    Init front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './css/App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+       <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+export default Home;
+
+function Home() {
+  return <>
+        
+    </>;
+}
